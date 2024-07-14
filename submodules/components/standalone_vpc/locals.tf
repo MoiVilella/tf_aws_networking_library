f9aa48a66c15834be78cfe1faa24387c22551e9f
Moises Vilella <MoiVilella@users.noreply.github.com>
2024-07-14 12:40:30 +0200

    Fix dynamic subnetting based on available AZs

--- submodules/components/standalone_vpc/locals.tf
+++ submodules/components/standalone_vpc/locals.tf
@@ -3,8 +3,9 @@ locals {
 
   # Subnetting locals
   cidr_mask       = split("/", var.m_cidr_block)[1]
-  subnet_count    = pow(2, (var.m_subneting_mask - local.cidr_mask))
-  subnets         = [for i in range(local.subnet_count) : cidrsubnet(var.m_cidr_block, 3, i)]
-  public_subnets  = slice(local.subnets, 0, 3)
-  private_subnets = slice(local.subnets, 3, 6)
+  newbits_mask    = var.m_subneting_mask - local.cidr_mask
+  subnet_count    = pow(2, local.newbits_mask)
+  subnets         = [for i in range(local.subnet_count) : cidrsubnet(var.m_cidr_block, local.newbits_mask, i)]
+  public_subnets  = slice(local.subnets, 0, length(local.region_availability_zones))
+  private_subnets = slice(local.subnets, length(local.region_availability_zones), length(local.region_availability_zones) * 2)
 }
